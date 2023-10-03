f208ae75c1582edcc6796e05712cb7f8d274b4fd
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-10-04 02:14:54 +0300

    feat: added Steps section markup

---
+++ src/blocks/modules/index-page/app-section/app-section.js
@@ -0,0 +1,13 @@
+modules.define('app-section', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
