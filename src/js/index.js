4f1b867ce9c89cf0039b58c4ad326b32c04d59c1
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-10-03 11:49:06 +0300

    feat: created footer base markup

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
+let vh = window.innerHeight * 0.01;
+
+function setVhProperty() {
+  vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setVhProperty();
+
+window.addEventListener('resize', () => {
+  setVhProperty();
+});
+
 import './import/modules';
 import './import/components';
