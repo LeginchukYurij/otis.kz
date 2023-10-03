712956aa4ebd362b1ecb17b1a21c2bcfe5150376
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-10-04 00:57:15 +0300

    feat: added latest-news section markup

---
+++ src/blocks/modules/index-page/latest-news-section/latest-news-section.js
@@ -0,0 +1,13 @@
+modules.define('latest-news-section', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
