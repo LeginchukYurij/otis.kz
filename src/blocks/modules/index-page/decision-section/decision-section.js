e0e9e80f48c615b594af7a5f1eecc71ed2435529
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-10-04 00:08:26 +0300

    feat: added Decisions section markup

--- src/blocks/modules/index-page/decision-section/decision-section.js
+++ src/blocks/modules/index-page/decision-section/decision-section.js
@@ -1,13 +1,28 @@
-modules.define('decision-section', ['i-bem-dom'], function(provide, bemDom) {
+const tariffTabBtns = document.querySelectorAll(
+  '.tariff-card .tariff-card__tabs-btn'
+);
 
-provide(bemDom.declBlock(this.name, {
-    onSetMod: {
-        js: {
-            inited: function() {
-                
-            }
-        }
-    }
-}));
+if (tariffTabBtns.length) {
+  tariffTabBtns.forEach((tab) => {
+    tab.addEventListener('click', function (e) {
+      e.preventDefault();
 
-});
+      const card = this.closest('.tariff-card');
+      const tabNum = this.dataset.tab;
+
+      card.querySelectorAll('.tariff-card__tabs-btn').forEach((tab) => {
+        tab.classList.remove('active');
+      });
+
+      card.querySelectorAll('.tariff-card__tabs-cotnent').forEach((tab) => {
+        tab.classList.remove('active');
+      });
+
+      this.classList.add('active');
+
+      card
+        .querySelector(`.tariff-card__tabs-cotnent[data-tab='${tabNum}']`)
+        .classList.add('active');
+    });
+  });
+}
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,4 +1,5 @@
 import '%modules%/header/header';
 import '%modules%/footer/footer';
-import '%modules%/index-page/clients-section/clients-section';
+import '%modules%/index-page/decision-section/decision-section';
 import '%modules%/index-page/geo-section/geo-section';
+import '%modules%/index-page/clients-section/clients-section';
