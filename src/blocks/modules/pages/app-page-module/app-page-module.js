3e6f42bfaabe5c33340fa1c25c2c470743c9d5e9
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-10-12 00:22:07 +0300

    feat: app page ready

---
+++ src/blocks/modules/pages/app-page-module/app-page-module.js
@@ -0,0 +1,13 @@
+modules.define('app-page-module', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
