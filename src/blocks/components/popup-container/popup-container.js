002cea671c86ea62423956358f8f2dfbf97d0623
LeginchukYurij <leginchuk.yurij@gmail.com>
2023-10-12 17:16:32 +0300

    feat: product popup ready

---
+++ src/blocks/components/popup-container/popup-container.js
@@ -0,0 +1,43 @@
+const triggers = document.querySelectorAll('[data-trigger-modal]');
+
+if(triggers.length) {
+  triggers.forEach((trigger) => {
+    trigger.addEventListener('click', function(e) {
+      e.preventDefault();  
+      const target = this.dataset.triggerModal;
+
+      const popup = document.querySelector(`.popup[data-target='${target}']`);
+      
+      if(popup) {
+        popup.classList.add('active');
+        document.querySelector('html').classList.add('noscroll');
+      }
+    });
+  });
+}
+
+const closers = document.querySelectorAll('.popup .close-popup');
+
+if(closers.length) {
+  closers.forEach((closer) => {
+    closer.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.closest('.popup').classList.remove('active');
+      document.querySelector('html').classList.remove('noscroll');
+    });
+  });
+}
+
+const popups = document.querySelectorAll('.popup');
+
+if(popups.length) {
+  popups.forEach((popup) => {
+    popup.addEventListener('click', function(e) {
+      console.log(this.closest('.popup__inner'));
+      if(!e.target.closest('.popup__inner')) {
+        e.target.closest('.popup').classList.remove('active');
+        document.querySelector('html').classList.remove('noscroll');
+      }
+    });
+  });
+}
--- src/js/import/components.js
+++ src/js/import/components.js
@@ -1,2 +1,3 @@
 import '%components%/nice-select/nice-select';
 import '%components%/accordion-item/accordion-item';
+import '%components%/popup-container/popup-container';
